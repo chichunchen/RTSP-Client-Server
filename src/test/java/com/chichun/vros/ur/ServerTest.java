9199486360c046de1ef4628f0ebb7cdf8181ce9b
chichunchen <chichunchen844@gmail.com>
2018-06-26 21:17:05 -0400

    Port to maven for using jcodec

---
+++ src/test/java/com/chichun/vros/ur/ServerTest.java
@@ -0,0 +1,20 @@
+package com.chichun.vros.ur;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Server.
+ */
+public class ServerTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
